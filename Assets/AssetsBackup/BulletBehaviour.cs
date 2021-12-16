43066f6203886b000ff581768c90ee80caab12bb
Eak <banyapol9676@gmail.com>
2021-12-17 00:48:51 +0700

    first commit

---
+++ Assets/AssetsBackup/BulletBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+
+    public float bulletSpeed;
+    public float secondsUntilDestroyed;
+    public float damage;
+    public float color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody ourRigidBody = GetComponent<Rigidbody>();
+        ourRigidBody.velocity = transform.forward * bulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        secondsUntilDestroyed -= Time.deltaTime;
+
+        if (secondsUntilDestroyed < 1)
+        {
+            transform.localScale *= secondsUntilDestroyed;
+        }
+
+        if (secondsUntilDestroyed < 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision thisCollision)
+    {
+
+        GameObject theirGameObject = thisCollision.gameObject;
+
+        if (theirGameObject.GetComponent<EnemyBehaviour>() != null)
+        {
+            HealthSystem theirHealthSystem = theirGameObject.GetComponent<HealthSystem>();
+            if (theirHealthSystem != null)
+            {
+                theirHealthSystem.TakeDamage(damage);
+            }
+
+            Destroy(gameObject);
+        }
+
+    }
+
+}
